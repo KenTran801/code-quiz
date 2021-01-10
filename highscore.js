0eaa2a4814eb61b2c346778c5c1cddf562a6d59c
KenTran801 <utes.fan.tran@gmail.com>
2021-01-10 15:12:48 -0700

    Figured out and finsihed locale stroage coding.

--- highscore.js
+++ highscore.js
@@ -18,13 +18,19 @@ var highScores = JSON.parse(window.localStorage.getItem("highScores")) || [];
 
 // put names and scores into list
 if (highScores !== "") {
+    // sort function to rank the scores from highest to lowest, shoutout to BCS learning for the help
+    highScores.sort(function(a, b) {
+        return parseInt(b.score) - parseInt(a.score);
+    });
+    // loop to pull in scores/names
     for (let i = 0; i < highScores.length; i++) {
         var listCreate = document.createElement("li");
         listCreate.setAttribute("class", "listCreate");
         listCreate.setAttribute("style", "font-weight: bold; padding: 5px")
         listCreate.textContent = highScores[i].name + " " + highScores[i].score;
         highscoreEl.appendChild(listCreate);
-    }
+    };
+
 }
 
 // Using previous for loop for reference
